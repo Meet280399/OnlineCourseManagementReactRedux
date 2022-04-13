4953628cdc36cfbe3f7f1c51d1c42cf7bda8f170
Jonny <meett.patel@avm.edu.in>
2022-04-13 21:59:47 +0530

    done for the evaluation

--- spec/suite/InstructorService.spec.js
+++ spec/suite/InstructorService.spec.js
@@ -1,67 +1,65 @@
-// import Instructor from "../../src/model/Instructor.js";
-// import InstructorService from "../../src/service/InstructorService.js";
+import Instructor from "../../src/model/Instructor.js";
+//import { addInstructor } from "../../src/redux/instructor/instructorActions";
+import InstructorService from "../../src/service/InstructorService.js";
 
-// describe("InstructorService", () => {
-//   describe("find Instructor", () => {
-//     it("Testing find instructor by id funciton.", () => {
-//       let service = new InstructorService();
-//       service.findInstructorById(201).then((result) => {
-//         let instructor = result.data;
-//         expect(instructor.name).toBe("Tapan Dandawala");
-//       });
-//     });
-//   });
+describe("InstructorService", () => {
+  describe("find Instructor", () => {
+    it("return the instructor by id", () => {
+      let service = new InstructorService();
+      service.findInstructorById(201).then((result) => {
+        let instructor = result.data;
+        expect(instructor.name).toBe("Tapan Dandawala");
+      });
+    });
+  });
 
-//   describe("add Instructor", () => {
-//     let service = new InstructorService();
-//       let inst = new Instructor();
-//       inst.instructorId = 202;
-//       inst.name = "Aryan Thakran";
-//       inst.email = "aryan.thakran@gmail.com";
-//       inst.mobileNo = 8790005227;
-//       inst.salary = 78000;
-//       inst.grades = 90;
-//     it("Testing Add instructor funciton.", () => {
-//       // console.log("inside test " + JSON.stringify(inst));
-//       service.addInstructor(inst).then((result) => {
-//         let instructor = result.data;
-//         expect(instructor.name).toBe("Aryan Thakran")
-//         // expect(employee.department.departmentName).toBe('HR');
-//       });
-//     });
-//   });
+  describe("add Instructor", () => {
+    it("add instructor ", () => {
+      let service = new InstructorService();
+      let inst = new Instructor();
+      inst.instructorId = 5;
+      inst.name = "akhila";
+      inst.email = "akhila@gmail.com";
+      inst.mobileNo = 7896421598;
+      inst.salary = 20000;
+      inst.grades = 50;
 
-//   describe("update Instructor", () => {
-//     it("Testing Update Instructor  by id funciton.", () => {
-//       let service = new InstructorService();
+      service.addInstructor(inst).then((result) => {
+        let instructor = result.data;
+      });
+    });
+  });
 
-//       let inst = new Instructor();
-//       inst.instructorId = 10;
-//       inst.name = "Akhil";
-//       inst.email = "Ashwini@gmail.com";
-//       inst.mobileNo = 8639297191;
-//       inst.salary = 20000;
-//       inst.grades = 60;
+  describe("upadate Instructor", () => {
+    it("update instructor", () => {
+      let service = new InstructorService();
+      let inst = new Instructor();
+      inst.instructorId = 5;
+      inst.name = "monika";
+      inst.email = "akhila@gmail.com";
+      inst.mobileNo = 7896421598;
+      inst.salary = 20000;
+      inst.grades = 50;
 
-//       service.updateInstructor(inst).then((result) => {
-//         service.findInstructorById(10).then((result) => {
-//           let instructor = result.data;
-//           expect(instructor.name).toBe("Akhil");
-//         });
-//       });
-//     });
-//   });
+      service.updateInstructor(inst).then((result) => {
+        service.findInstructorById(5).then((result) => {
+          let instructor = result.data;
+          expect(instructor.name).toBe("monika");
+        });
+      });
+    });
+  });
 
-//   describe("delete Instructor", () => {
-//     it("Testing delete Instructor by id funciton.", () => {
-//       let service = new InstructorService();
-//       let instId = 10;
-//       service.deleteInstructorById(instId).then(() => {
-//         service.findInstructorById(instId).then((result) => {
-//           let instructor = result.data;
-//           expect(null).toBe(null);
-//         });
-//       });
-//     });
-//   });
-// });
+  describe("delete Instructor", () => {
+    it("detete instructor", () => {
+      let service = new InstructorService();
+      let instId = 5;
+      service.deleteInstructorById(5).then(() => {
+        service.findInstructorById(instId).then((result) => {
+          let instructor = result.data;
+          expect(null).toBe(null);
+        });
+      });
+    });
+  });
+});
